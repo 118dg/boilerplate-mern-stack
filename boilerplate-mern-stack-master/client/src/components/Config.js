2c61b375f0bac65614c5e04d8da44943a430a067
118dg <54856465+118dg@users.noreply.github.com>
2021-07-03 13:22:14 +0900

    key

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -3,7 +3,8 @@ export const USER_SERVER = '/api/users';
 
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+//export const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+export const API_KEY = '6fc649b044b463a61640e5943654c490';
 export  const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
 
 
